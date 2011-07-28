fa3ea0a1680f1db167daf1eff90e3952add41e83
linlingyu <linlingyu@baidu.com>
2011-07-28 17:42:29 +0800

    kjsadhakjshdjsa

--- src/import.php
+++ src/import.php
@@ -1,31 +1,4 @@
-<?php
-/*
- * Tangram
- * Copyright 2009 Baidu Inc. All rights reserved.
- * 
- * path: import.php
- * author: berg
- * version: 1.0
- * date: 2010/07/18 23:57:52
- *
- * @fileoverview * import.js的php版本
- * 接受一个f参数，格式和import.js相同，自动合并js并输出
- * 此外，接受一个path参数，用逗号分隔，如果在当前目录下找不到需要的js文件，会依次去path目录找
- * 此外，本脚本支持引入一个包所有文件（其实也就是一个目录下的所有js文件，**不递归**）
- * IE下，get请求不能超过2083字节，请注意。
- */
 
-//$DEBUG = true;
-$DEBUG = false;
-
-$MATCHED = array();
-
-$IMPORTED = array();
-
-if(isset($_GET['path'])){
-    $PATH = explode(',', $_GET['path']);
-}else{
-    $PATH = array();
 }
 
 //如果从path里面找不到，就去上级目录的tangram中找
